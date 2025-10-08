52691e20fd561c4481a86cf772cf643b30e7bf17
Allen-pogi <143931506+Allen-pogi@users.noreply.github.com>
2025-10-08 13:33:46 +0800

    Update header.jsx

--- src/user/components/header.jsx
+++ src/user/components/header.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [dark, setDark] = useState(() => {
@@ -42,22 +43,24 @@ const Header = () => {
       </div>
 
       <div cclassName="flex items-center">
-        <div className="flex items-center gap-4 text-slate-800 dark:text-white ml-auto justify-start ">
-          <div className="h-6 w-6">
-            <svg
-              className="text-primary"
-              fill="none"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4 4H17.3334V17.3334H30.6666V30.6666H44V44H4V4Z"
-                fill="currentColor"
-              />
-            </svg>
+        <Link to="/" className="flex items-center gap-2">
+          <div className="flex items-center gap-4 text-slate-800 dark:text-white ml-auto justify-start ">
+            <div className="h-6 w-6">
+              <svg
+                className="text-primary"
+                fill="none"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 4H17.3334V17.3334H30.6666V30.6666H44V44H4V4Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
+            <h2 className="text-lg font-bold">PrimeTrack</h2>
           </div>
-          <h2 className="text-lg font-bold">PrimeTrack</h2>
-        </div>
+        </Link>
       </div>
       {/* Logo */}
     </header>
