e7a0c5a1840c2c554352ce9503cef9d0e51b110f
Allen-pogi <143931506+Allen-pogi@users.noreply.github.com>
2025-10-09 10:28:16 +0800

    Update scanner.jsx

--- src/scanner.jsx
+++ src/scanner.jsx
@@ -131,7 +131,7 @@ const HybridQRScanner = () => {
   return (
     <div className="min-h-screen bg-background-light dark:bg-background-dark">
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-background-light dark:bg-background-dark">
+      <div className="flex flex-col items-center  min-h-screen p-4 bg-background-light dark:bg-background-dark">
         <h1 className="text-2xl font-bold text-black dark:text-white mb-4">
           Hybrid QR Scanner
         </h1>
