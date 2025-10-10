d82a295a89775446d4aa71ab88a1820d26949ded
Allen-pogi <143931506+Allen-pogi@users.noreply.github.com>
2025-10-10 14:34:53 +0800

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:5000";
-const API_BASE_URL = "https://d15ab02d2c1b.ngrok-free.app";
+const API_BASE_URL = "https://137ecabc15fb.ngrok-free.app ";
 export default API_BASE_URL;
